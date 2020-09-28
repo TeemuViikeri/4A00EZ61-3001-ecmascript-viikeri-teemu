f8a105b8a5ee19b16f77da6b3fd237b616b11f19
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-09-28 23:12:31 +0300

    Add finished e01 files.

---
+++ homework/2020-40/e01/index.js
@@ -0,0 +1,30 @@
+function makeCalculation () {
+  function asynFunc (resolve, reject) {
+    setTimeout(() => {
+      console.log('calculating stuff')
+      const division = 5 / 5
+      resolve(division)
+    }, 1000)
+  }
+  const p = new Promise(asynFunc)
+  return p
+}
+
+// make the calculation that takes time and when ready, invoke the
+// arrow function.
+// makeCalculation().then(result => console.log(`result = ${result}`))
+
+function sendStuffToBackend (result) {
+  function asynFunc (resolve, reject) {
+    setTimeout(() => {
+      console.log(`sending to backend ${result}`)
+      resolve('done')
+    }, 1000)
+  }
+  const p = new Promise(asynFunc)
+  return p
+}
+
+makeCalculation()
+  .then(sum => sendStuffToBackend(sum))
+  .then(msg => console.log(msg))
